ee4f8fb4a3aef6f04b7dacdcbb9efd849e774cf6
Oliver Stenbom <oliver@stenbom.eu>
2025-08-28 17:06:36 +0200

    Format with biome

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental',
-    clientSegmentCache: true,
-  }
+	experimental: {
+		ppr: "incremental",
+		clientSegmentCache: true,
+	},
 };
 
 export default nextConfig;
