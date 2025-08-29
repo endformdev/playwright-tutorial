31d6c24c1f188439cd1da07ffceeedd1a3a2586f
Oliver Stenbom <oliver@stenbom.eu>
2025-08-29 16:40:01 +0200

    Initial completion of stage 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
 	testDir: "./tests",
+	globalSetup: "./global-setup.ts",
+	globalTeardown: "./global-teardown.ts",
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,8 +28,8 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "https://endform-playwright-tutorial.vercel.app",
-		// baseURL: "http://localhost:3000",
+		// baseURL: "https://endform-playwright-tutorial.vercel.app",
+		baseURL: "http://localhost:3000",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
