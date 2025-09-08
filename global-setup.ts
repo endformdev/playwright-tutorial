f5076ee56c51f387a4f169f57436cc4f10002ea8
Oliver Stenbom <oliver@stenbom.eu>
2025-09-08 09:34:43 +0200

    Can correctly create api cookie storage state file

--- global-setup.ts
+++ global-setup.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import type { FullConfig } from "@playwright/test";
 
 export default async function globalSetup(config: FullConfig) {
@@ -18,10 +19,35 @@ export default async function globalSetup(config: FullConfig) {
 			},
 			body: JSON.stringify({ email, password }),
 		});
-		const data = await response.json().catch(() => null);
-		console.log("[globalSetup] create user status:", response.status);
-		console.log("[globalSetup] create user body:", data);
+		const data = (await response.json()) as { session: string };
+		if (!data.session) {
+			throw new Error("Failed to create user");
+		}
+
+		writeSessionFile(data.session, baseURL);
 	} catch (error) {
 		console.error("[globalSetup] error creating user:", error);
 	}
+}
+
+function writeSessionFile(session: string, baseURL: string) {
+	fs.writeFileSync(
+		".auth/shared-api-user.json",
+		JSON.stringify({
+			cookies: [
+				{
+					name: "session",
+					value: session,
+					domain: new URL(baseURL).hostname,
+					path: "/",
+					// Playwright will delete the cookie when the test is done
+					expires: -1,
+					httpOnly: true,
+					secure: true,
+					sameSite: "Lax",
+				},
+			],
+			origins: [],
+		}),
+	);
 }
