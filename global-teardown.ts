314835f7176137a1a5f43471a590027292f7bf16
Oliver Stenbom <oliver@stenbom.eu>
2025-09-08 11:13:39 +0200

    Pull out create user functions into helper file

--- global-teardown.ts
+++ global-teardown.ts
@@ -1,4 +1,5 @@
 import type { FullConfig } from "@playwright/test";
+import { deleteUser } from "./setup-utils";
 
 export default async function globalTeardown(config: FullConfig) {
 	const { baseURL } = config.projects[0]?.use || {};
@@ -6,21 +7,5 @@ export default async function globalTeardown(config: FullConfig) {
 		throw new Error("baseURL is required");
 	}
 
-	const email = "pw-global@example.com";
-
-	try {
-		const response = await fetch(`${baseURL}/api/internal/user`, {
-			method: "DELETE",
-			headers: {
-				"content-type": "application/json",
-				authorization: "Bearer VerySecretDummyToken",
-			},
-			body: JSON.stringify({ email }),
-		});
-		const data = await response.json().catch(() => null);
-		console.log("[globalTeardown] delete user status:", response.status);
-		console.log("[globalTeardown] delete user body:", data);
-	} catch (error) {
-		console.error("[globalTeardown] error deleting user:", error);
-	}
+	await deleteUser(baseURL, "api");
 }
