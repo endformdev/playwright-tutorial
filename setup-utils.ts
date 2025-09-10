0951eec37ebd5d45a45b43a0205480c292dea173
Oliver Stenbom <oliver@stenbom.eu>
2025-09-10 15:05:08 +0200

    Add fixture for generating new users for specific tests

--- setup-utils.ts
+++ setup-utils.ts
@@ -1,43 +1,64 @@
 import fs from "node:fs";
 
-export async function createUser(baseURL: string, userKindName: string) {
+export interface ApiUser {
+	email: string;
+	password: string;
+	session: string;
+}
+
+export async function createUser(baseURL: string): Promise<ApiUser> {
 	const email = `${Math.random().toString(36).substring(2, 15)}@example.com`;
 	const password = "testpassword123";
 
+	const response = await fetch(`${baseURL}/api/internal/user`, {
+		method: "POST",
+		headers: {
+			"content-type": "application/json",
+			authorization: "Bearer VerySecretDummyToken",
+		},
+		body: JSON.stringify({ email, password }),
+	});
+	const data = (await response.json()) as { session: string };
+	if (!data.session) {
+		throw new Error("Failed to create user");
+	}
+
+	return { email, password, session: data.session };
+}
+
+export async function deleteUser(baseURL: string, email: string) {
+	const response = await fetch(`${baseURL}/api/internal/user`, {
+		method: "DELETE",
+		headers: {
+			"content-type": "application/json",
+			authorization: "Bearer VerySecretDummyToken",
+		},
+		body: JSON.stringify({ email }),
+	});
+	const data = await response.json().catch(() => null);
+
+	return data;
+}
+
+export async function createUserInFile(baseURL: string, userKindName: string) {
 	try {
-		const response = await fetch(`${baseURL}/api/internal/user`, {
-			method: "POST",
-			headers: {
-				"content-type": "application/json",
-				authorization: "Bearer VerySecretDummyToken",
-			},
-			body: JSON.stringify({ email, password }),
-		});
-		const data = (await response.json()) as { session: string };
-		if (!data.session) {
-			throw new Error("Failed to create user");
-		}
+		const { email, session } = await createUser(baseURL);
 
 		writeEmailFile(email, `.auth/${userKindName}-user-email.txt`);
-		writeSessionFile(data.session, baseURL, `.auth/${userKindName}-user.json`);
+		writeSessionFile(session, baseURL, `.auth/${userKindName}-user.json`);
 	} catch (error) {
 		console.error("[setup-utils] error creating user:", error);
 	}
 }
 
-export async function deleteUser(baseURL: string, userKindName: string) {
+export async function deleteUserFromFile(
+	baseURL: string,
+	userKindName: string,
+) {
 	const email = readEmailFile(`.auth/${userKindName}-user-email.txt`);
 
 	try {
-		const response = await fetch(`${baseURL}/api/internal/user`, {
-			method: "DELETE",
-			headers: {
-				"content-type": "application/json",
-				authorization: "Bearer VerySecretDummyToken",
-			},
-			body: JSON.stringify({ email }),
-		});
-		const data = await response.json().catch(() => null);
+		await deleteUser(baseURL, email);
 		// console.log("[setup-utils] delete user status:", response.status);
 		// console.log("[setup-utils] delete user body:", data);
 		fs.unlinkSync(`.auth/${userKindName}-user-email.txt`);
@@ -45,6 +66,20 @@ export async function deleteUser(baseURL: string, userKindName: string) {
 	} catch (error) {
 		console.error("[setup-utils] error deleting user:", error);
 	}
+}
+
+export function getCookieForSession(session: string, baseURL: string) {
+	return {
+		name: "session",
+		value: session,
+		domain: new URL(baseURL).hostname,
+		path: "/",
+		// Playwright will delete the cookie when the test is done
+		expires: -1,
+		httpOnly: true,
+		secure: true,
+		sameSite: "Lax" as const,
+	};
 }
 
 function writeEmailFile(email: string, filePath: string) {
@@ -59,19 +94,7 @@ function writeSessionFile(session: string, baseURL: string, filePath: string) {
 	fs.writeFileSync(
 		filePath,
 		JSON.stringify({
-			cookies: [
-				{
-					name: "session",
-					value: session,
-					domain: new URL(baseURL).hostname,
-					path: "/",
-					// Playwright will delete the cookie when the test is done
-					expires: -1,
-					httpOnly: true,
-					secure: true,
-					sameSite: "Lax",
-				},
-			],
+			cookies: [getCookieForSession(session, baseURL)],
 			origins: [],
 		}),
 	);
