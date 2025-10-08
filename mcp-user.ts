e6a97e0a34e4e8cf3ad4317b7bd40aff623274e2
Oliver Stenbom <oliver@stenbom.eu>
2025-10-08 09:35:52 +0200

    Modify MCP user script to only need TSX and node.

--- mcp-user.ts
+++ mcp-user.ts
@@ -2,21 +2,23 @@
 
 import { createUserInFile, deleteUserFromFile } from "./setup-utils";
 
-const scriptArg = process.argv[2];
+(async () => {
+	const scriptArg = process.argv[2];
 
-if (scriptArg !== "create" && scriptArg !== "delete") {
-	console.error(
-		`Error: Invalid argument "${scriptArg}". Expected "create" or "delete".`,
-	);
-	process.exit(1);
-}
+	if (scriptArg !== "create" && scriptArg !== "delete") {
+		console.error(
+			`Error: Invalid argument "${scriptArg}". Expected "create" or "delete".`,
+		);
+		process.exit(1);
+	}
 
-const baseURL = "https://endform-playwright-tutorial.vercel.app";
-// const baseURL = "http://localhost:3000";
+	const baseURL = "https://endform-playwright-tutorial.vercel.app";
+	// const baseURL = "http://localhost:3000";
 
-if (scriptArg === "create") {
-	console.log("Creating MCP user", baseURL);
-	await createUserInFile(baseURL, "mcp");
-} else if (scriptArg === "delete") {
-	await deleteUserFromFile(baseURL, "mcp");
-}
+	if (scriptArg === "create") {
+		console.log("Creating MCP user", baseURL);
+		await createUserInFile(baseURL, "mcp");
+	} else if (scriptArg === "delete") {
+		await deleteUserFromFile(baseURL, "mcp");
+	}
+})();
