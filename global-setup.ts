0c22b6a23b839afc60c6431892b8f0095f736b17
Oliver Stenbom <oliver@stenbom.eu>
2026-05-20 12:58:50 +0200

    Update deps

--- global-setup.ts
+++ global-setup.ts
@@ -1,12 +1,6 @@
-import type { FullConfig } from "@playwright/test";
+import { baseURL } from "./playwright.config";
 import { createUserInFile } from "./setup-utils";
 
-export default async function globalSetup(config: FullConfig) {
-	const { baseURL } = config.projects[0]?.use || {};
-
-	if (!baseURL) {
-		throw new Error("baseURL is required");
-	}
-
+export default async function globalSetup() {
 	await createUserInFile(baseURL, "api");
 }
