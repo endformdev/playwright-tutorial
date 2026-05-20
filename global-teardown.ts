30ad6f78975ee25b77b14d79280ce6488bf10ca6
Oliver Stenbom <oliver@stenbom.eu>
2026-05-20 12:59:00 +0200

    Update deps

--- global-teardown.ts
+++ global-teardown.ts
@@ -1,11 +1,6 @@
-import type { FullConfig } from "@playwright/test";
+import { baseURL } from "./playwright.config";
 import { deleteUserFromFile } from "./setup-utils";
 
-export default async function globalTeardown(config: FullConfig) {
-	const { baseURL } = config.projects[0]?.use || {};
-	if (!baseURL) {
-		throw new Error("baseURL is required");
-	}
-
+export default async function globalTeardown() {
 	await deleteUserFromFile(baseURL, "api");
 }
