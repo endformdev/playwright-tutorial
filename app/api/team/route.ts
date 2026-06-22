24e46cc72d129dce3e65a3f13fbf6771ec930634
Oliver Stenbom <oliver@stenbom.eu>
2026-06-22 16:43:10 +0200

    Can inject more application level faults

--- app/api/team/route.ts
+++ app/api/team/route.ts
@@ -1,6 +1,17 @@
 import { getTeamForUser } from "@/lib/db/queries";
+import { isFaultActive } from "@/lib/faults";
 
 export async function GET() {
+	if (await isFaultActive("api-team-malformed-json")) {
+		return new Response("{", {
+			status: 200,
+			headers: {
+				"content-type": "application/json",
+				"x-fault-injected": "api-team-malformed-json",
+			},
+		});
+	}
+
 	const team = await getTeamForUser();
 	return Response.json(team);
 }
